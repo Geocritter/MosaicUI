342a7794ea1e0cb29e93c67e432b73a64fbaabfe
Tony Wang <tony.wangs@gmail.com>
2020-11-10 12:09:50 -0500

    creating home

--- mosui/src/App.js
+++ mosui/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+// Dependables
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+// Components
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                </Switch>
+        </Router>
+        
       </header>
     </div>
   );
---
+++ mosui/src/components/Home/Home.js
@@ -0,0 +1,23 @@
+// Dependables
+import React from 'react';
+import './Home.css';
+
+
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+            MosUI
+        </p>
+        <a>
+            Simple UI tool to extract images from GEE
+        </a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default Home;
